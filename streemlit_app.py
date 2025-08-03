8a0653fad3bb9a8ef643673c90a0fd3840b8b9d4
rcyga <rcygan@proton.me>
2025-08-03 03:33:45 +0100

    Update streemlit_app.py

--- streemlit_app.py
+++ streemlit_app.py
@@ -14,7 +14,12 @@ conn = st.connection("snowflake")
 session = conn.session()
 from snowflake.snowpark.functions import col
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+# convert Snowpark dataframe to Pandas dataframe to use the loc function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
